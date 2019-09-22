b10e4e9a12c5f61c38d8c165c778b45f2d0cd21c
Teerawut Sararam <jamesjames@MacBook-Pro-khxng-Teerawut.local>
2019-09-22 18:18:25 +0700

    Commit Final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Content from './Content';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 const word = "Hello";
+
+
 class App extends Component {
- render() {
- return (
- <div>
- {
- Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
- }
- </div>
- );
- }
+  render() {
+
+    return (
+      <div className="bg">
+      
+        <div className="App">
+        <div className="text">
+          Lab 03 - react
+          
+        </div>
+        <div className="text">
+          Teerawut Sararam
+        </div>
+          
+          <WordCard value="James" />
+          <WordCard value="5935512098" />
+          
+        </div>
+      </div>
+      
+    );
+  }
 }
+
 export default App;
